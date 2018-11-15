f67390f0b6ab7e7518ee319531da5b4c681fa8ab
worldown <d_dexter@mail.ru>
2018-11-15 15:08:05 +0300

    1.3. JavaDoc. Code convention[#86708]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+	* Calculate
+	*
+	* @author Kosolapov Ilya (d_dexter@mail.ru)
+*/
 public class Calculate {
+	/**
+	* Entry point
+	* @param args 
+	*/
 	public static void main(String[] args) {
 			System.out.println("Hello World");
 	}
