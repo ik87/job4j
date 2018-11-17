5dbd88b1ca54041d37be8815ec16411c8cd60304
worldown <d_dexter@mail.ru>
2018-11-17 14:22:45 +0300

    3.1. Элементарный калькулятор.[#86699]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,29 +1,31 @@
 package ru.job4j.calculate;
 /**
-	* Calculate
-	*
-	* @author Kosolapov Ilya (d_dexter@mail.ru)
-	* @version $id$
-	* @since 0.1
-*/
+ * Calculate
+ *
+ * @author Kosolapov Ilya (d_dexter@mail.ru)
+ * @version $id$
+ * @since 0.1
+ */
 public class Calculate {
-	/**
-	* Main
-	* @param args - args.
-	*/
-	public static void main(String[] args) {
-			System.out.println("Hello World");
-	}
-	
-	
-	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
-	public String echo(String name) {
-			return "Echo, echo, echo: " + name;
-	}
+    private double result;
+
+    public double getResult() {
+        return this.result;
+    }
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    public void sub(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void mult(double first, double second) {
+        this.result = first * second;
+    }
+
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
 }
-
-
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -4,19 +4,71 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CalculateTest {
-	/**
-	* Test.
-	*
-	* @author Petr Arsentev (parsentev@yandex.ru)
-	* @version $ID$
-	* @since 0.1
-	*/
-	@Test
-	public void whenTakeNameThenTreeEchoPlusName() {
-		String input = "Petr Arsentev";
-		String expect = "Echo, echo, echo: Petr Arsentev";
-		Calculate calc = new Calculate();
-		String result = calc.echo(input);
-		assertThat(result, is(expect));
-	}
+    /**
+     * Test.
+     *
+     * @author Petr Arsentev (parsentev@yandex.ru)
+     * @version $ID$
+     * @since 0.1
+     */
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+
+        Calculate calc = new Calculate();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expect = 2D;
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Test.
+     *
+     * @author Kosolapov Ilya (d_dexter@mail.ru)
+     * @version $ID$
+     * @since 0.1
+     */
+    @Test
+    public void whenSubTwoMinusOneThenOne() {
+
+        Calculate calc = new Calculate();
+        calc.sub(2D, 1D);
+        double result = calc.getResult();
+        double expect = 1D;
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Test.
+     *
+     * @author Kosolapov Ilya (d_dexter@mail.ru)
+     * @version $ID$
+     * @since 0.1
+     */
+    @Test
+    public void whenMultTwoByThreeThenSix() {
+
+        Calculate calc = new Calculate();
+        calc.mult(2D, 3D);
+        double result = calc.getResult();
+        double expect = 6D;
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Test.
+     *
+     * @author Kosolapov Ilya (d_dexter@mail.ru)
+     * @version $ID$
+     * @since 0.1
+     */
+    @Test
+    public void whenDivSixByTwoThenThree() {
+
+        Calculate calc = new Calculate();
+        calc.div(6D, 2D);
+        double result = calc.getResult();
+        double expect = 3D;
+        assertThat(result, is(expect));
+    }
 }
