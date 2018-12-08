9c9fabb860d339bde8189748935549e667354e0f
worldown <d_dexter@mail.ru>
2018-12-08 18:55:14 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#86744]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest2.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest2.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
+
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -18,11 +20,24 @@ import static org.junit.Assert.assertThat;
  * @version $ID$
  * @since 0.1
  */
-public class StartUITest {
+public class StartUITest2 {
 
     private Tracker tracker;
     private final PrintStream stdout = System.out;
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final String menu =
+            new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                    .add("Menu:")
+                    .add("0.Add new item")
+                    .add("1.Show all items")
+                    .add("2.Edit item")
+                    .add("3.Delete item")
+                    .add("4.Find item by id")
+                    .add("5.Find item by name")
+                    .add("6.Exit program")
+                    .toString();
+
+    private StringBuilder body;
 
     @Before
     public void init() {
@@ -38,6 +53,10 @@ public class StartUITest {
 
         //change print output
         System.setOut(new PrintStream(out));
+
+        //init StringBuilder body
+        body = new StringBuilder();
+        body.append(menu);
     }
 
     @After
@@ -72,38 +91,55 @@ public class StartUITest {
     @Test
     public void whenShowAll() {
         String[] action = {"1", "6"};
-        StringBuilder expected = new StringBuilder();
+        body.append(String.format("------------------------------ Show all requests -----------------------------%n"));
+        body.append(String.format("%-20s%-11s%-25s%n", "Id", "Name", "Description"));
+        body.append(String.format("------------------------------------------------------------------------------%n"));
         for (Item item : tracker.findAll()) {
-            expected.append(
+            body.append(
                     String.format("%-20s%-11s%-25s%n",
                             item.getId(), item.getName(), item.getDesc()));
         }
+        body.append(String.format("------------------------------------------------------------------------------%n"));
+        body.append(menu);
         new StartUI(new StubInput(action), tracker).init();
-        assertThat(new String(out.toByteArray()).contains(expected), is(true));
+        // assertThat(new String(out.toByteArray()).contains(expected), is(true));
+        assertThat(out.toString(), is(body.toString()));
+
     }
 
     @Test
     public void whenFindByNameThenShowMatches() {
         String[] action = {"5", "test name", "6"};
-        StringBuilder expected = new StringBuilder();
+        body.append(System.lineSeparator());
+        body.append(String.format("-------------------------------- Find by name --------------------------------%n"));
+        body.append(String.format("%-20s%-11s%-25s%n", "Id", "Name", "Description"));
+        body.append(String.format("------------------------------------------------------------------------------%n"));
         for (Item item : tracker.findByName("test name")) {
-            expected.append(
+            body.append(
                     String.format("%-20s%-11s%-25s%n",
                             item.getId(), item.getName(), item.getDesc()));
         }
+        body.append(String.format("------------------------------------------------------------------------------%n"));
+        body.append(menu);
         new StartUI(new StubInput(action), tracker).init();
-        assertThat(out.toString().contains(expected), is(true));
+        assertThat(out.toString(), is(body.toString()));
     }
 
     @Test
     public void whenFindByIdThenShow() {
         String id = tracker.findAll()[3].getId();
         String[] action = {"4", id, "6"};
+        body.append(System.lineSeparator());
+        body.append(String.format("----------------------------- Find by Id -------------------------------------%n"));
+        body.append(String.format("%-20s%-11s%-25s%n", "Id", "Name", "Description"));
+        body.append(String.format("------------------------------------------------------------------------------%n"));
         Item item = tracker.findById(id);
-        String expected = String.format("%-20s%-11s%-25s%n",
-                item.getId(), item.getName(), item.getDesc());
+        body.append(String.format("%-20s%-11s%-25s%n",
+                item.getId(), item.getName(), item.getDesc()));
+        body.append(String.format("------------------------------------------------------------------------------%n"));
+        body.append(menu);
         new StartUI(new StubInput(action), tracker).init();
-        assertThat(out.toString().contains(expected), is(true));
+        assertThat(out.toString(), is(body.toString()));
     }
 
 }
