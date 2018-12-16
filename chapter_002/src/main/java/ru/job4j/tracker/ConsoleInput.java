998c7782986c2b0bb299cbbb2cbf6fdc46f79124
worldown <d_dexter@mail.ru>
2018-12-16 14:56:58 +0300

    3. if-else-throw[#86752]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -27,11 +27,10 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of range");
         }
+        return key;
     }
 
 }
--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -41,10 +41,10 @@ public class StubInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of range");
         }
+        return key;
+
     }
 }
