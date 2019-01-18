08c1909ace0d18ce7638c826baa503261709626f
worldown <d_dexter@mail.ru>
2019-01-18 10:33:17 +0300

    Конвертация ArrayList в двухмерный массив[#86734]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -0,0 +1,27 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+/**
+ * @author Kosolapov Ilya (d_dexter@mail.ru)
+ * @version $0.1$
+ * @since 0.1
+ */
+public class ConvertList2Array {
+    public int[][] toArray(List<Integer> list, int rows) {
+        int cells = (int) Math.ceil(list.size() / (double) rows);
+        int i = 0;
+
+        int[][] array = new int[rows][cells];
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cells; x++) {
+                if (i < list.size()) {
+                    array[y][x] = list.get(i++);
+                } else {
+                    array[y][x] = 0;
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertList2ArrayTest {
+    @Test
+    public void when7ElementsThen9() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                3
+        );
+        int[][] expect = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when11ElementsThen12() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
+                3
+        );
+        int[][] expect = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 0}
+        };
+        assertThat(result, is(expect));
+    }
+}
