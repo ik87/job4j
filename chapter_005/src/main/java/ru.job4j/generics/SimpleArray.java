f3073eef9fae3e5e55c1e671f54a22b890ad8c96
worldown <d_dexter@mail.ru>
2019-03-18 13:56:25 +0300

    Реализовать SimpleArray<T> [#156]

---
+++ chapter_005/src/main/java/ru.job4j/generics/SimpleArray.java
@@ -0,0 +1,99 @@
+package ru.job4j.generics;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Example simple generic collection with fixed size
+ *
+ * @param <T> any type
+ * @author Kosolapov Ilya (d_dexter@mail.ru)
+ * @version 0.1
+ * @since 18.03.2019
+ */
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] data;
+    private Integer count = 0;
+
+    public SimpleArray(int size) {
+        this.data = new Object[size];
+    }
+
+    /**
+     * add element
+     * @param element any element
+     * @throws ArrayStoreException if arrived max size for array
+     */
+    public void add(T element) {
+        if (count == data.length) {
+            throw new ArrayStoreException();
+        }
+        data[count++] = element;
+    }
+
+    /**
+     * get element by index
+     * @param index index element
+     * @return T element
+     */
+    public T get(int index) {
+        return (T) data[index];
+    }
+
+    /**
+     * replace element in the index position
+     * @param index position for new element
+     * @param element any element
+     */
+    public void set(int index, T element) {
+        data[index] = element;
+    }
+
+    /**remove element and decrement 'count'
+     *
+     * @param index index remove element
+     * @throws IndexOutOfBoundsException if index more then array length or less then 0
+     */
+    public void remove(int index) {
+        if (index >= data.length && index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        System.arraycopy(data, index + 1, data, index, data.length - 1 - index);
+        count--;
+
+    }
+
+    /**
+     * get current size element in array
+     * @return count
+     */
+    public Integer getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(data, count));
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < count;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) data[index++];
+            }
+        };
+    }
+}
---
+++ chapter_005/src/test/java/ru.job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,114 @@
+package ru.job4j.generics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Kosolapov Ilya (d_dexter@mail.ru)
+ */
+public class SimpleArrayTest {
+
+    @Test
+    public void whenAddThenSame() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(15);
+        /**
+         * add 1-5
+         */
+        for (int i = 0; i < 5; i++) {
+            simpleArray.add(i + 1);
+        }
+        assertThat(simpleArray.toString(), is("[1, 2, 3, 4, 5]"));
+        assertThat(simpleArray.getCount(), is(5));
+    }
+
+    @Test
+    public void whenGetElementThen3() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(15);
+        /**
+         * add 1-5
+         */
+        for (int i = 0; i < 5; i++) {
+            simpleArray.add(i + 1);
+        }
+
+        assertThat(simpleArray.get(2), is(3));
+    }
+
+    @Test
+    public void checkCount() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(5);
+        assertThat(simpleArray.getCount(), is(0));
+        simpleArray.add(3);
+        assertThat(simpleArray.getCount(), is(1));
+    }
+
+    @Test
+    public void whenRemoveOneElementThenEmpty() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(1);
+        simpleArray.add(1);
+        simpleArray.remove(0);
+        Assert.assertThat(simpleArray.toString(), is("[]"));
+        Assert.assertThat(simpleArray.getCount(), is(0));
+    }
+
+    @Test
+    public void whenRemoveInTheMiddleThenRemoved() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(15);
+        /**
+         * add 1-5
+         */
+        for (int i = 0; i < 5; i++) {
+            simpleArray.add(i + 1);
+        }
+        simpleArray.remove(2);
+        Assert.assertThat(simpleArray.toString(), is("[1, 2, 4, 5]"));
+        Assert.assertThat(simpleArray.getCount(), is(4));
+    }
+
+    @Test
+    public void whenSetNewElementThenChanged() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(15);
+        /**
+         * add 1-5
+         */
+        for (int i = 0; i < 5; i++) {
+            simpleArray.add(i + 1);
+        }
+        simpleArray.set(3, 22);
+        assertThat(simpleArray.toString(), is("[1, 2, 3, 22, 5]"));
+    }
+
+    @Test
+    public void checkIterator() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(15);
+        /**
+         * add 1-5
+         */
+        for (int i = 0; i < 5; i++) {
+            simpleArray.add(i + 1);
+        }
+        Iterator<Integer> it = simpleArray.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void checkIteratorException() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(1);
+        simpleArray.add(1);
+        Iterator<Integer> it = simpleArray.iterator();
+        it.next();
+        it.next();
+    }
+}
