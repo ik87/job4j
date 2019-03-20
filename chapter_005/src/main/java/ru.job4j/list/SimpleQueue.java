33bbfac0487320e161a9ab72a763835422600afc
worldown <d_dexter@mail.ru>
2019-03-20 21:14:47 +0300

    5.3.3.1 Очередь на двух стеках [#160] #3

--- chapter_005/src/main/java/ru.job4j/list/SimpleQueue.java
+++ chapter_005/src/main/java/ru.job4j/list/SimpleQueue.java
@@ -2,6 +2,7 @@ package ru.job4j.list;
 
 /**
  * This is the simple queue (FIFO) is based on the two simple stacks
+ *
  * @param <E>
  */
 public class SimpleQueue<E> {
@@ -10,31 +11,25 @@ public class SimpleQueue<E> {
 
     /**
      * an element put on the top one stack after put existing elements
+     *
      * @param element some element
      */
     public void push(E element) {
-        swap(aStack, bStack);
         aStack.push(element);
     }
 
     /**
      * get a last element
+     *
      * @return top element
      */
     public E pop() {
-        swap(bStack, aStack);
-        return bStack.pop();
-    }
-
-    /**
-     * swap elements
-     * @param a first stack
-     * @param b second stack
-     */
-    private void swap(SimpleStack<E> a, SimpleStack<E> b) {
-        while (b.getCount() > 0) {
-            a.push(b.pop());
+        if (bStack.getCount() == 0) {
+            while (aStack.getCount() > 0) {
+                bStack.push(aStack.pop());
+            }
         }
+        return bStack.pop();
     }
 
     /**
@@ -44,4 +39,6 @@ public class SimpleQueue<E> {
     public int getCount() {
         return aStack.getCount() + bStack.getCount();
     }
+
+
 }
