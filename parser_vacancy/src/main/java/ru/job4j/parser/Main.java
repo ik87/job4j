dfdc9f95738567adcf56d53192491b25bbf0c01b
worldown <d_dexter@mail.ru>
2019-05-06 08:29:49 +0300

    Парсер вакансий на sql.ru [#1731] #3

---
+++ parser_vacancy/src/main/java/ru/job4j/parser/Main.java
@@ -0,0 +1,15 @@
+package ru.job4j.parser;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ConvertDate convertDate = new ConvertDate();
+        Map<String, Vacancy> vacancyMap = new LinkedHashMap<>();
+        Parser parser = new Parser();
+    }
+
+}
