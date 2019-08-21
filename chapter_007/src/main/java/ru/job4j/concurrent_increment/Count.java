45e5e92451f2f3421422c853261813089548c490
Kosolapov Ilya <d_dexter@mail.ru>
2019-08-21 21:12:29 +0300

    класс Count и метод int incremant()

--- chapter_007/src/main/java/ru/job4j/concurrent_increment/Count.java
+++ chapter_007/src/main/java/ru/job4j/concurrent_increment/Count.java
@@ -1,4 +1,19 @@
 package ru.job4j.concurrent_increment;
 
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
 }
--- chapter_007/src/test/java/ru/job4j/concurrent_increment/CountTest.java
+++ chapter_007/src/test/java/ru/job4j/concurrent_increment/CountTest.java
@@ -1,5 +1,42 @@
+package ru.job4j.concurrent_increment;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class CountTest {
+    /**
+     * Класс описывает нить со счетчиком.
+     */
+    private class ThreadCount extends Thread {
+        private final Count count;
 
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        //Создаем счетчик.
+        final Count count = new Count();
+        //Создаем нити.
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        //Запускаем нити.
+        first.start();
+        second.start();
+        //Заставляем главную нить дождаться выполнения наших нитей.
+        first.join();
+        second.join();
+        //Проверяем результат.
+        assertThat(count.get(), is(2));
+
+    }
 }
