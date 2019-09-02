cde684f2752fd1c4bced45c176ce3c9bc2c07091
worldown <d_dexter@mail.ru>
2019-09-02 12:34:49 +0300

    Обеспечить остановку потребителя

---
+++ chapter_007/src/main/java/ru/job4j/producer_consumer/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.producer_consumer;
+/**
+ * Interrupt consumer thread
+ *
+ * @author Kosolapov Ilya (d_dexter@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+    }
+}
