32105961b4b9860d1c7cff4c33008cb8745fb1c2
worldown <d_dexter@mail.ru>
2019-09-16 22:15:18 +0300

    ExecutorService рассылка почты.

---
+++ chapter_007/src/main/java/ru/job4j/email_sending_services/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.email_sending_services;
+/**
+ * @author Kosolapov Ilya (d_dexter@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+    String name;
+    String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
