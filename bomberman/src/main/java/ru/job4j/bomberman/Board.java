b96e68448dbcaa5d9c472cd31088b7f78433192a
worldown <d_dexter@mail.ru>
2019-10-02 18:19:17 +0300

    Игра бомбермен #12

--- bomberman/src/main/java/ru/job4j/bomberman/Board.java
+++ bomberman/src/main/java/ru/job4j/bomberman/Board.java
@@ -1,9 +1,14 @@
 package ru.job4j.bomberman;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.job4j.bomberman.figures.Cell;
 import ru.job4j.bomberman.figures.Hero;
 
+import java.util.List;
+import java.util.SplittableRandom;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 
 /**
@@ -16,9 +21,12 @@ import java.util.concurrent.locks.Lock;
  * @since 0.1
  */
 public class Board {
+    private static final Logger LOG = LogManager.getLogger(Board.class.getName());
+
     private final Lock[][] board;
     private final Hero hero;
     private ExecutorService pool;
+
 
     public Board(Lock[][] board, Hero hero, ExecutorService pool) {
         this.board = board;
@@ -27,6 +35,69 @@ public class Board {
     }
 
     public void move(Cell source, Cell dest) {
-        pool.submit(()->hero.move(dest, board));
+        pool.submit(() -> {
+            SplittableRandom random = new SplittableRandom();
+            //Generate way
+            Cell[] way = hero.way(hero.getPosition(), dest, random.nextBoolean());
+            //set lock on current position
+            lock(board, hero.getPosition()).lock();
+
+            LOG.debug("Generate way {}", List.of(way));
+
+            for (int i = 1; i < way.length; i++) {
+                Cell currentStep = way[i - 1];
+                Cell nextStep = way[i];
+                try {
+                    if (lock(board, nextStep).tryLock(500, TimeUnit.MILLISECONDS)) {
+                        //LOG.debug(currentStep);
+                        hero.setPosition(nextStep.getX(), nextStep.getY());
+                        LOG.debug(hero.getPosition());
+                        lock(board, currentStep).unlock();
+                        Thread.sleep(1000);
+                    } else {
+                        //Check where collision happened on X or Y line
+                        //If it was vertical collision then move hero on 1 step left or right
+                        //If it was horizontal collision then move hero on 1 step Up or Down
+
+                        boolean vertical = false;
+                        Cell newSource;
+                        if (currentStep.getY() != nextStep.getY()) {
+                            newSource = hero.goLeftOrRight(currentStep, board.length, random.nextBoolean());
+                            vertical = true;
+                        } else {
+                            newSource = hero.goUpOrDown(currentStep, board[0].length, random.nextBoolean());
+                        }
+                        //recalculate way from current position
+                        way = hero.way(newSource, dest, vertical);
+
+                        //concat current position with way
+                        way = concat(currentStep, way);
+                        i = 0;
+                        LOG.debug("Generate new way: {}", List.of(way));
+                    }
+                } catch (InterruptedException e) {
+                    LOG.error(e.getMessage());
+                }
+            }
+        });
+    }
+
+
+    /**
+     * Helper method. Get lock object
+     */
+    private Lock lock(Lock[][] board, Cell cell) {
+        return board[cell.getY()][cell.getX()];
+    }
+
+
+    /**
+     * Add current step at begin array
+     */
+    private Cell[] concat(Cell cell, Cell[] tmp) {
+        Cell[] way = new Cell[tmp.length + 1];
+        way[0] = cell;
+        System.arraycopy(tmp, 0, way, 1, tmp.length);
+        return way;
     }
 }
--- bomberman/src/main/java/ru/job4j/bomberman/figures/Hero.java
+++ bomberman/src/main/java/ru/job4j/bomberman/figures/Hero.java
@@ -1,14 +1,7 @@
 package ru.job4j.bomberman.figures;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ru.job4j.bomberman.Board;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SplittableRandom;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 import java.util.function.Consumer;
 
 /**
@@ -18,7 +11,6 @@ import java.util.function.Consumer;
  */
 public class Hero {
 
-    private static final Logger LOG = LogManager.getLogger(Board.class.getName());
 
     private final Cell position;
 
@@ -26,69 +18,15 @@ public class Hero {
         return position;
     }
 
+    public void setPosition(int x, int y) {
+        this.position.x = x;
+        this.position.y = y;
+    }
+
     public Hero(final Cell position) {
         this.position = position;
     }
 
-
-    /**
-     * Main method that make calculating hero movement
-     *
-     * @param dest
-     * @param board
-     */
-    public void move(Cell dest, Lock[][] board) {
-
-        //Fast and thread safe random (instead classical Random)
-        SplittableRandom random = new SplittableRandom();
-        /**
-         * Generate wayGenerator
-         */
-        Cell[] way = wayGenerator(position, dest, random.nextBoolean());
-
-        LOG.debug("Generate way {}", List.of(way));
-
-        //set lock on current position
-        board[position.y][position.x].lock();
-
-        for (int i = 1; i < way.length; i++) {
-            Cell currentStep = way[i - 1];
-            Cell nextStep = way[i];
-            try {
-                if (lock(board, nextStep).tryLock(500, TimeUnit.MILLISECONDS)) {
-                    LOG.debug(currentStep);
-                    position.x = nextStep.x;
-                    position.y = nextStep.y;
-                    lock(board, currentStep).unlock();
-                    Thread.sleep(1000);
-                } else {
-                    boolean vertical = false;
-                    Cell newSource;
-                    //Check where collision happened on X or Y line
-                    if (currentStep.getY() != nextStep.getY()) {
-                        //If it was vertical collision then move hero on 1 step left or right
-                        newSource = goLeftOrRight(currentStep, board.length, random.nextBoolean());
-                        vertical = true;
-                    } else {
-                        newSource = goUpOrDown(currentStep, board[0].length, random.nextBoolean());
-                    }
-                    //recalculate way from current position
-                    way = wayGenerator(newSource, dest, vertical);
-
-                    //Concat current position with way
-                    way = concat(currentStep, way);
-
-                    i = 0;
-                    LOG.debug("Generate new way: {}", List.of(way));
-                }
-            } catch (InterruptedException e) {
-                LOG.error(e.getMessage());
-            }
-
-        }
-        LOG.debug(way[way.length - 1]);
-
-    }
 
     /**
      * Go at one step UP or Down from current position
@@ -98,7 +36,7 @@ public class Hero {
      * @param direction where is he go? If true then UP if false then DOWN
      * @return new position
      */
-    private Cell goUpOrDown(Cell currentStep, int length, boolean direction) {
+    public Cell goUpOrDown(Cell currentStep, int length, boolean direction) {
         Cell newSource;
         if (currentStep.getY() + 1 < length && currentStep.getY() - 1 >= 0) {
             newSource = direction
@@ -121,7 +59,7 @@ public class Hero {
      * @param direction where is he go? If true then LEFT if false then RIGHT
      * @return new position
      */
-    private Cell goLeftOrRight(Cell currentStep, int length, boolean direction) {
+    public Cell goLeftOrRight(Cell currentStep, int length, boolean direction) {
         Cell newSource;
         if (currentStep.getX() + 1 < length && currentStep.getX() - 1 >= 0) {
             newSource = direction
@@ -135,30 +73,6 @@ public class Hero {
         return newSource;
     }
 
-    /**
-     * Helper method. Get lock object
-     *
-     * @param board
-     * @param cell
-     * @return
-     */
-    private Lock lock(Lock[][] board, Cell cell) {
-        return board[cell.getY()][cell.getX()];
-    }
-
-    /**
-     * Add current step at begin array
-     *
-     * @param cell
-     * @param tmp
-     * @return
-     */
-    private Cell[] concat(Cell cell, Cell[] tmp) {
-        Cell[] way = new Cell[tmp.length + 1];
-        way[0] = cell;
-        System.arraycopy(tmp, 0, way, 1, tmp.length);
-        return way;
-    }
 
     /**
      * Calculate way from source to dest position.
@@ -177,7 +91,7 @@ public class Hero {
      *
      * @return array calculated way
      */
-    private Cell[] wayGenerator(Cell source, Cell dest, boolean vertical) {
+    public Cell[] way(Cell source, Cell dest, boolean vertical) {
         List<Cell> steps = new ArrayList<>();
         //set motion vector
         int vecX = source.x <= dest.x ? 1 : -1;
