3c4f48fc5ec4f2a2fcfe762fb8e863e5812ad487
worldown <d_dexter@mail.ru>
2019-11-25 21:45:04 +0300

    Перенести все виды из предыдущего задания на JSP

---
+++ chapter_008/src/main/java/ru/job4j/web_app_arhitecture_jsp/presentation/UserCreateServlet.java
@@ -0,0 +1,23 @@
+package ru.job4j.web_app_arhitecture_jsp.presentation;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * This servlet is responsible for create User
+ *
+ * @author Kosolapov Ilya (d_dexter@mail.ru)
+ * @version $id
+ * @since 0.1
+ */
+public class UserCreateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("create.jsp").forward(req, resp);
+    }
+
+}
